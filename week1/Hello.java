453905a47dafd426bacf816335b3c05de8a424b2
HelanGeorge <helangeorge999@gmail.com>
2024-04-14 12:49:02 +0545

    week 1 commit 3

--- week1/Hello.java
+++ week1/Hello.java
@@ -13,7 +13,67 @@ I am on module ST4003CEM
 
 System.out.print (not println) and produces the same output. */
 
-         System.out.println("");
+         System.out.println("This is my first program \n \nI am on module ST4003CEM ");
+
+         /*3. Write a program that prints the following output:
+
+A "quoted" String is
+
+'much' better if you learn
+
+the rules of "escape sequences."
+
+Also, "" represents an empty
+
+String. Don't forget: use \"
+
+instead of " !
+
+'' is not the same as " */
+        
+        System.out.println("A \"quoted\" String is \n \n\'much\' better if you learn \n \nrules of \"escape sequences.\" \n\nAlso, \"\" represents an empty \n\nString. Don't forget: use \\\" \n\nInstead of \" !\n\n\'\' is not the same as \" ");
+      
+        /*4. WAP that prints the following pattern:
+
+*
+
+**
+
+***
+
+**** */
+        System.out.println("* \n\n** \n\n*** \n\n**** ");
+        /*5. WAP prints the following pattern:
+
+*********
+
+*********
+
+*********
+
+********* */
+        System.out.println("********* \n\n********* \n\n********* \n\n*********");
+
+        /*6. WAP that prints the following output:
+
+A well-formed Java program has a main method with { and } braces.
+
+
+
+A System.out.println statement has ( and ) and usually a String that starts and ends with a " character.
+
+(But we type \" instead!)
+
+What is the difference between
+
+a ' and a "?  Or between a " and a \"?
+
+
+
+One is what we see when we're typing our program. The other is what appears on the "console." */
+
+        System.out.println("A well-formen Java program has a main methos with { and } braces. \n\n\n\nA System.out.println statement has ( and ) and usually a String that starts and ends with a \" character. \n\n(But we type \\\" instead!) \n\nWhat is the difference between \n\na \' and a\"? Or between a \" and a\\\"? \n\n\n\nOne is what we see when we\'re typing our program. The other is what appears on the \"console.\"");
+
     }
     
 }
