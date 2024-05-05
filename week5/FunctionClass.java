f46ed086e9f68ca61915ef3f20b66fca8f95a03d
HelanGeorge <helangeorge999@gmail.com>
2024-05-05 11:39:29 +0545

    updating codes

--- week5/FunctionClass.java
+++ week5/FunctionClass.java
@@ -4,6 +4,18 @@ public class FunctionClass {
        System.out.println("This is funciton parts");
     
    }
+   /*In the function void means nothing/null */
+
+   /*With return type as int */
+   int returnInteger(){
+       int returnThis =1;
+       return returnThis; 
+   }
+   /*with return and params */
+   boolean returnString(int num1, int num2){
+       boolean check = num1 == num2;
+       return check;
+   }
        public static void main(String[] args) {
            /*To se a fucntion you need to mae object
             * note: just rememeber the syntax for now
