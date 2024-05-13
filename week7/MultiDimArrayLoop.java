8b1c5608613e7c2dc6bb956a41ae2feb56285868
helangeorge999 <helangeorge999@gmail.com>
2024-05-13 13:56:18 +0545

    final commit of may 13

--- week7/MultiDimArrayLoop.java
+++ week7/MultiDimArrayLoop.java
@@ -22,11 +22,46 @@ public class MultiDimArrayLoop {
            */
           /* Loop outer array */
           int outerLength = arr.length; // ->4
-          for (int j = 0; j < outerLength; j++) {
-              System.out.println("Array in index " + j);
-              System.out.println(arr[j][0]);
-              System.out.println(arr[j][1]);
+          for (int outer = 0; outer < outerLength; outer ++) {
+              System.out.println("Array in index " + outer);
+            //   System.out.println(arr[outer][0]);
+            //   System.out.println(arr[outer][1]);
+            int innerArrayLength = arr[outer][1];
+            for (int inner = 0; inner < innerArrayLength; inner ++ ){
+                System.out.println(arr [outer][inner]); // inner loops from 0,1
+             }
             }
-  
+
+
+
+
+            /*Make an array of string names 
+             * [
+             *    ["George", "Niraj", "Subham"], ->0
+             *    ["Rijan", "Ram", "Vardaer"], ->1
+             * ]
+             */
+             
+             /*Loop each names */
+            /*Task 2 on the same loop, the on which array is oscar in */
+            /*Answer should be 1 */
+            // String[][] names = new String [2][3];
+
+            String[][] name = {{"George", "Niraj", "Subham"}, {"Rijan", "Ram", "Vardaer"}};
+            
+            name[0][0] = "George";
+            name[0][1] = "Niraj";
+            name[0][2] = "Subham";
+            name[1][0] = "Rijan";
+            name[1][1] = "Ram";
+            name[1][2] = "Vardae";
+
+            for (int i = 0; i < name.length; i++) {
+                System.out.println();
+                for (int j = 0; j < name[i].length; j++) {
+                    // System.out.print(name[i][j] + "\t\t ");
+                    if (name[i][j] == "Ram") {
+                        System.out.println(i + " " + j);
+                }
     }
 }
